8fbe4f2d1d92a529a471d3ee024e802378bafed9
Spoorthi M <mspoorthi9972@gmail.com>
2026-03-23 19:36:28 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,37 @@
+
+# loop pattern
+
+'''n=int(input("enter the no:"))
+for i in range(n):
+    for j in range(n):
+        print("*",end='')
+print()'''
+
+'''n=int(input("enter the no:"))
+for i in range(n):
+    print("*"*n)/'''
+
+#increasing triangle
+
+'''n=int(input("enter the no:"))
+for i in range(n):
+    print("*"*(i+1))'''
+
+
+#reverse
+'''n=int(input("enter the no:"))
+for i in range(n):
+    print("*"*(n-i))'''
+
+#right aligned triangle stars
+'''n=int(input("enter the no:"))
+for i in range(n):
+    print("*"*(n-i-1))'''
+
+
+'''n=int(input("enter the no:"))
+for i in range(n):
+    for j in range(1,n-i+1):'''
+        
+
+
